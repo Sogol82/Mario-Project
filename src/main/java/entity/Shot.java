cd415ec9f86364f72c1d83b9d99beee75b4f0d90
Sogol <so.zamani182@gmail.com>
2023-05-24 14:56:07 +0330

    to check branches

--- src/main/java/entity/Shot.java
+++ src/main/java/entity/Shot.java
@@ -15,14 +15,11 @@ public class Shot extends Entity {
     CollisionChecker collisionChecker;
     public int SHeight;
     public boolean stop;
-    public int numberOfFPS;
-    public int counter = 0;
     public int shotsTileColumn;
     public int distanceFromFirstColumn = 0;
     public int drawCounter = 1;
-    public int firstXDrawn = 0;
-    ///////////////lastXDrawn initialization is for fun
-    public int lastXDrawn = 1;
+    public int firstXDrawn;
+    public int lastXDrawn;
     public Shot(int x, int y, int speed,String direction, GamePanel gp) {
 
         shotsTileColumn = x/Data.tileSize;
@@ -33,31 +30,21 @@ public class Shot extends Entity {
         this.gp = gp;
         collisionChecker = new CollisionChecker(gp);
 
-
         if(direction.equals("right") || direction.equals("upRight")) {
             this.direction = "right";
 //            distanceFromFirstColumn += Data.tileSize;
             shotsTileColumn += 1;
-
+            distanceFromFirstColumn = x%Data.tileSize;
         } else {
             this.direction = "left";
-
+            distanceFromFirstColumn = Data.tileSize - (x%Data.tileSize);
         }
-
-//        if(this.direction.equals("right")) {
-//            this.x+=Data.tileSize;
-//        } else {
-//            this.x-=Data.tileSize;
-//        }
 
         SHeight = Data.tileSize/2;
         solidArea = new Rectangle(0,2 * Data.scale,SHeight,(SHeight) - (5 * Data.scale));
 
-        numberOfFPS = ((Data.tileSize*8 - (SHeight - solidArea.x - solidArea.width))/speed)*2;
-
         getShotImage();
     }
-
     void getShotImage() {
         try {
             upR = ImageIO.read(getClass().getResourceAsStream("/marioImages/Shot.png"));
@@ -66,63 +53,10 @@ public class Shot extends Entity {
             e.printStackTrace();
         }
     }
-
-    public void moveForward() {
-        if(direction.equals("right")) {
-            x += speed;
-            if(!collisionChecker.checkRight(this)) {
-                x -= speed;
-                stop = true;
-            }
-        } else {
-            x -= speed;
-            if(!collisionChecker.checkLeft(this)) {
-                x += speed;
-                stop = true;
-            }
-        }
-        check8Block();
-
-//        if(direction.equals("right")) {
-//            x += speed;
-//        } else {
-//            x -= speed;
-//        }
-//        check8Block();
-
-    }
-
-    public void moveBackward() {
-//        if(direction.equals("right")) {
-//            x -= speed;
-//            if(!collisionChecker.checkRight(this)) {
-//                x += speed;
-//                stop = true;
-//            }
-//        } else {
-//            x += speed;
-//            if(!collisionChecker.checkLeft(this)) {
-//                x -= speed;
-//                stop = true;
-//            }
-//        }
-//        check8Block();
-
-        if(direction.equals("right")) {
-            x -= speed;
-        } else {
-            x += speed;
-        }
-        check8Block();
-    }
-
-
     int shotCounter = 0;
     public void draw(int x, Graphics2D g2) {
 //        this.x = x + gp.player.x - gp.player.screenX;
         check8Block();
-
-        counter++;
         BufferedImage image = null;
         switch(direction) {
             case "right":
@@ -164,7 +98,6 @@ public class Shot extends Entity {
 //            g2.drawImage(image, screenX, y, Data.tileSize, PHeight, null);
 //        }
     }
-
     public void check8Block() {
 
         if(direction.equals("right")) {
@@ -174,8 +107,8 @@ public class Shot extends Entity {
             }
         } else {
             if(firstXDrawn - lastXDrawn >= Data.tileSize*8) {
-                System.out.println("hehe");
-                System.out.println("first x : " + firstXDrawn + "   last x : " + lastXDrawn + "  difference : " + (firstXDrawn-lastXDrawn));
+//                System.out.println("hehe");
+//                System.out.println("first x : " + firstXDrawn + "   last x : " + lastXDrawn + "  difference : " + (firstXDrawn-lastXDrawn));
                 stop = true;
                 return;
             }
@@ -205,18 +138,4 @@ public class Shot extends Entity {
 //        stop = false;
 //        return true;
     }
-
-
-
-
-
-
-
-
-
-
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////
-
 }
