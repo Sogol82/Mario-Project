67d82ae0c128a35e492819dbaa027718362c699c
Sogol <so.zamani182@gmail.com>
2023-05-25 20:56:23 +0330

    Fix collision of Mega and Fire Mario with tiles and coins and adding Interface of player

---
+++ src/main/java/entity/PlayerI.java
@@ -0,0 +1,19 @@
+package entity;
+
+import management.Data;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public interface PlayerI {
+    void getPlayerImage(int id);
+    void setDefaultValues();
+    void update();
+    void fallUpdate();
+    void deadUpdate();
+    void plantCollisionUpdate();
+    void gravityUpdate();
+    void draw(Graphics2D g2);
+}
